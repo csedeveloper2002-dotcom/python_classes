b10e985337b6097a476566ea09147d1df5b95c3d
csedeveloper2002-dotcom <csedeveloper2002@gmail.com>
2025-12-26 16:24:47 +0530

    Create separate_odd_even.py

---
+++ separate_odd_even.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even = []
+odd = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+
+print("Even numbers:", even)
+print("Odd numbers:", odd)
