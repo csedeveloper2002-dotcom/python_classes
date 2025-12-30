ff065b51c9e596fdd3e317c24948b29331386ad4
csedeveloper2002-dotcom <csedeveloper2002@gmail.com>
2025-12-30 11:37:24 +0530

    Create max_subarray.py

---
+++ max_subarray.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input("Enter array elements: ").split()))
+
+max_sum = arr[0]
+curr_sum = arr[0]
+
+for i in range(1, len(arr)):
+    curr_sum = max(arr[i], curr_sum + arr[i])
+    max_sum = max(max_sum, curr_sum)
+
+print("Maximum subarray sum:", max_sum)
